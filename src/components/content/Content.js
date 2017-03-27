5ebb4a44429f33a563c4e3da6b46d4eb16f95d7b
Nedjim <nedjim.danimon@gmail.com>
2017-03-27 14:18:35 +0200

    add fin de parties

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -7,9 +7,11 @@ export default class Content extends Component {
     constructor(){
         super();
         this.state = {
+            totalTours : 5,
+            tours: 0,
             players : {
-                'Gaby' : {name: 'Gaby', score : 0, choise: null},
-                'Jayden': {name:'Jayden', score : 0, choise: null}
+                'Gaby' : {name: 'Gaby', score : 0, choise: null, tours: 0},
+                'Jayden': {name:'Jayden', score : 0, choise: null, tours: 0}
             }
         }
     }
@@ -18,16 +20,38 @@ export default class Content extends Component {
         let players = this.state.players;
 
         players[name].choise = value;
-        this.setState({players : players})
-        console.log(this.state.players)
+        players[name].tours += 1;
+
+        this.setState({players : players});
+        this.stopTours();
+    }
+
+    stopTours(){
+        let players = this.state.players;
+        let tours1 = players['Gaby'].tours;
+        let tours2 = players['Jayden'].tours;
+
+        if(tours1 == tours2){
+            console.log('Fin du tour');
+            this.setState({tours: this.state.tours += 1});
+        }
     }
     
     render(){
-        return (
-            <div>
-                 <Player key={1} player={this.state.players['Gaby']} getChoise={this.getChoise.bind(this)}/>
-                 <Player key={2} player={this.state.players['Jayden']} getChoise={this.getChoise.bind(this)}/>
-            </div>
-        )
+         if(this.state.tours == this.state.totalTours){
+            return (
+                <div>
+                    <h1>FIN du jeu</h1>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div>
+                    <Player key={1} player={this.state.players['Gaby']} getChoise={this.getChoise.bind(this)}/>
+                    <Player key={2} player={this.state.players['Jayden']} getChoise={this.getChoise.bind(this)}/>
+                </div>
+            )
+        }
     }
 }
